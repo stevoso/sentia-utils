c202db20f37b86044f0059dd3289f5483b5c0cf1
Števo Šoltýs <stevo.soltys@gmail.com>
2021-12-14 14:00:44 +0100

    bugfix

--- WebUtil.php
+++ WebUtil.php
@@ -2,19 +2,15 @@
 namespace Sentia\Utils;
 
 class WebUtil {
-    /**
-     * rozbali ZIP subor do urceneho adresara
-     * @param $zipFile - cesta k zip suboru
-     * @param $dir - adresar (bez koncoveho lomitka), kde sa ma zip odzipovat
-     * @param bool $overwrite
-     * @return bool|null
-     */
-    public function getOriginFromReferer(string $urlReferer):string{
+
+    public function getOriginFromReferer(?string $urlReferer):string{
         $origin = '';
-        $urlArr = parse_url($urlReferer);
-        $origin .= $urlArr['scheme'].'://';
-        $origin .= $urlArr['host'];
-        $origin .= isset($urlArr['port']) ? ':'.$urlArr['port'] : '';
+        if($urlReferer !== null){
+            $urlArr = parse_url($urlReferer);
+            $origin .= $urlArr['scheme'].'://';
+            $origin .= $urlArr['host'];
+            $origin .= isset($urlArr['port']) ? ':'.$urlArr['port'] : '';
+        }
         return $origin;
     }
 }
